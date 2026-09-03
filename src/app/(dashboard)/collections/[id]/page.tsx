97369a7629e0e925e548387400bf0b0bef826fac
afrianns <hanifnafrian12@gmail.com>
2026-09-03 22:16:04 +0700

    updated deployed issues pt.1

--- src/app/(dashboard)/collections/[id]/page.tsx
+++ src/app/(dashboard)/collections/[id]/page.tsx
@@ -1,9 +1,29 @@
+"use server"
+
 import Main from "@/components/project-detail/Main";
+import { getProjectDetailById } from "@/services/projects.service";
 import { PagePropsType } from "@/types/types";
+import { notFound } from "next/navigation";
 
+interface PagePropsParamsType extends PagePropsType  { 
+    params: Promise<{ id: string }>
+}
 
-export default function DetailPage({searchParams}: PagePropsType) {
+export default async function DetailPage({params}: PagePropsParamsType) {
+    
+    const { id } = await params
+
+    const result = await getProjectDetailById(id)
+    
+    if(result.status == 404) notFound()
+
     return (
-        <Main searchParams={searchParams} />
+        <>
+            {(result.status == 200 && result.data) &&
+                <>
+                    <Main projectID={id} projectFromDB={result.data} />
+                </>
+            }
+        </>
     )
 } 
--- src/app/(dashboard)/home/[id]/page.tsx
+++ src/app/(dashboard)/home/[id]/page.tsx
@@ -12,37 +12,6 @@ interface PagePropsParamsType extends PagePropsType  {
 
 }
 
-// export let initialProject: WithPivotDataType = {
-//     id: 0,
-//     created_at: new Date(),
-//     uid: "",
-//     user_id: 0,
-//     type_id: 0,
-//     title: "",
-//     summary: "",
-//     visibility: "PUBLIC",
-//     disable_comments: false,
-//     client_name: "",
-//     updated_at: null,
-//     status_id: 0,
-//     Status: {
-//         id: 0,
-//         name: ""
-//     },
-//     Type: {
-//         id: 0,
-//         name: ""
-//     },
-//     Users: {
-//         id: 0,
-//         full_name: "",
-//         clerk_user_id: ""
-//     },
-//     Nodes: [],
-//     Project_tags: [],
-//     Project_tools: [],
-// }
-
 export default async function DetailPage({params}: PagePropsParamsType) {
     
     const { id } = await params
